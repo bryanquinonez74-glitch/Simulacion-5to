253c1d57447e42cbefa8340bb8eca8b33c627e90
Brallan A_Q_J <bryanquinonez74@gmail.com>
2025-10-15 13:08:11 -0600

    Delete Java FX  con Scenbuilder directory

--- Java FX  con Scenbuilder/PruebaAleatoriedad/src/main/java/com/example/pruebaaleatoriedad/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.pruebaaleatoriedad;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(PruebaAleatoriedadApplication.class, args);
-    }
-}
