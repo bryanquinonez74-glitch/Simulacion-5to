a46930402f83462d33ec9dd30f3612c6b4256632
Brallan A_Q_J <bryanquinonez74@gmail.com>
2025-10-15 13:08:21 -0600

    Delete Rstudio-ejercicios directory

--- Rstudio-ejercicios/R/pruebas estadisticas_Dependencia.R
+++
@@ -1,9 +0,0 @@
-# Datos de tabla 2x2
-tabla <- matrix(c(10,20,30,40), nrow=2, byrow=TRUE)
-rownames(tabla) <- c("Fila1","Fila2")
-colnames(tabla) <- c("Col1","Col2")
-
-# Realizar prueba Chi-cuadrado
-resultado <- chisq.test(tabla)
-print(resultado)
-
